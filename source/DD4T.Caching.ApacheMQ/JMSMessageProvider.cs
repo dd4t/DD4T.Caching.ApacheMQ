0f7f80c0d80a870fe1a1210c1963ae12dcd6aa79
Ferron <ferron.saan@trivident.com>
2018-09-12 09:52:47 +0200

    Update Core package reference

--- source/DD4T.Caching.ApacheMQ/JMSMessageProvider.cs
+++ source/DD4T.Caching.ApacheMQ/JMSMessageProvider.cs
@@ -8,9 +8,6 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DD4T.Caching.ApacheMQ
 {
